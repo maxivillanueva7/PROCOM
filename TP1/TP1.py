18c10b19df4b6408741ae9ea6e408d3db67ca851
Maxi_Villanueva <maucy7@gmail.com>
2026-05-21 11:31:11 -0300

    Cambiando valores del filtro a traves de comandos

--- TP1/TP1.py
+++ TP1/TP1.py
@@ -5,11 +5,46 @@ filtroprueba= RaisedCosineFilter(alpha=0.7, span=8, sps=8, rrc=True)
 print('Introduzca un comando')
 while True:
     comando= input(">")
+    if "=" in comando:
+        dividir= comando.split("=")
+        parametro= dividir[0]
+        valor=dividir[1]
 
-    if comando == "exit":
+        if parametro == "alpha":
+            valor = float(valor)
+            if valor<0 or valor>1:
+                print("Valor invalido. Un valor valido es 0<=Alpha<=1.")    
+            else:
+                filtroprueba.alpha = valor
+                print(f"Valor de alpha actualizado a {valor}")
+
+        elif parametro == "span":
+            valor = int(valor)
+            filtroprueba.span = valor
+            print(f"Valor de span actualizado a {valor}")
+
+        elif parametro == "sps":
+            valor = int(valor)
+            filtroprueba.sps = valor
+            print(f"Valor de sps actualizado a {valor}")
+
+        elif parametro == "rrc":
+            if valor == "True":
+                filtroprueba.rrc= True
+                print(f"Valor de rrc actualizado a True")
+            elif valor == "False":
+                filtroprueba.rrc= False
+                print(f"Valor de rrc actualizado a False")
+            else:
+                print("Valor invalido usar True o False")
+                
+    elif comando == "exit":
         break
+    elif comando == "generate":
+        filtroprueba.taps = filtroprueba._generate_filter()
+        print("Filtro generado nuevamente con parámetros cambiados")
     elif comando == "help":
-        print("Comandos disponibles son plot, coef y exit.")
+        print("Comandos disponibles son plot, coef,exit, generate, alpha=<float>, span=<int>, sps=<int> y rrc=<Bool>.")
     elif comando == "plot":
         filtroprueba.plot(time_domain=True, freq_domain=False)
     elif comando == "coef":
